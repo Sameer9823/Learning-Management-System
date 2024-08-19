a925b675cb7bfa2774076ab55badd492111e4402
Sameer9823 <sameerselokar924@gmail.com>
2024-08-19 12:11:36 +0530

    update url1

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 // Third-Party
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: 'https://lmsclient-three.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
