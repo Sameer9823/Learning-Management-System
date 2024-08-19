99c3def23be42cb5180fbbc584aba020d0952004
Sameer9823 <sameerselokar924@gmail.com>
2024-08-19 12:20:56 +0530

    update url2

--- client/src/Helper/axiosInstance.js
+++ client/src/Helper/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "ttps://learning-management-system-8.onrender.com/api/v1";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://learning-management-system-8.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
